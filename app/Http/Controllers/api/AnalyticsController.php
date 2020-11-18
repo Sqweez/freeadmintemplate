6d4b76c817bb32b8777ad9aef8d7292c34fad4f0
Sqweez <sasha.katerinin@gmail.com>
2020-11-18 11:30:39 +0600

    18/11/2020

--- app/Http/Controllers/api/AnalyticsController.php
+++ app/Http/Controllers/api/AnalyticsController.php
@@ -131,7 +131,7 @@ class AnalyticsController extends Controller
     public function getPartnerSales(Request $request) {
         $user_token = $request->get('user_token');
         if (!$user_token) {
-            return response()->json(['error' => 'Не передан идентификатор клиента']);
+            return response()->json(['error' => 'Не передан токен клиента']);
         }
         $client = Client::where('id', 3)
             ->with('partner_sales', 'partner_sales.products')
