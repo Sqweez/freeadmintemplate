c6a7bdaa097b20bb35e0fd727881be822eb239a4
Alexandr Katerinin <sasha.katerinin@gmail.com>
2022-06-30 17:35:58 +0600

    123

--- app/Http/Controllers/api/CategoryController.php
+++ app/Http/Controllers/api/CategoryController.php
@@ -64,7 +64,7 @@ class CategoryController extends Controller {
 
     public function indexShop() {
         return ShopCategoryResource::collection(
-            Category::where('is_site_visible', true)
+            Category::query()
                 ->with('seoText')
                 ->with('subcategories.seoText')
                 ->get()
