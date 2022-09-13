882113c6b21eef787e0876d45f9bb79c3f580c82
Alexandr Katerinin <sasha.katerinin@gmail.com>
2022-09-13 18:30:01 +0600

    news update

--- app/Http/Resources/shop/v2/NewsResource.php
+++ app/Http/Resources/shop/v2/NewsResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\shop\v2;
 
 use App\Http\Resources\shop\ProductsResource;
 use Carbon\Carbon;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Str;
 
@@ -12,11 +13,19 @@ class NewsResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  Request  $request
      * @return array
      */
     public function toArray($request)
     {
+
+        $products = collect([]);
+        $this->productNews->each(function ($item) use (&$products) {
+            collect($item->products)->each(function ($product) use (&$products){
+                $products->push($product);
+            });
+        });
+
         return [
             'id' => $this->id,
             'title' => $this->title,
@@ -25,7 +34,7 @@ class NewsResource extends JsonResource
             'short_text' => $this->short_text,
             'slug' => Str::slug($this->title) . '_' . Carbon::parse($this->created_at)->format('d.m.Y'),
             'date' => Carbon::parse($this->created_at)->format('d.m.Y'),
-            'products' => ProductsResource::collection($this->products),
+            'products' => ProductsResource::collection($products),
         ];
     }
 }
