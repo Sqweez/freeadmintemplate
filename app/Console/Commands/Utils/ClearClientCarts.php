ddace5e381b54c188ffd2850a1ffe4a90e14ab30
Alexandr Katerinin <sasha.katerinin@gmail.com>
2023-02-26 23:24:45 +0600

    clear carts

---
+++ app/Console/Commands/Utils/ClearClientCarts.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands\Utils;
+
+use App\Cart;
+use Illuminate\Console\Command;
+
+class ClearClientCarts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'clear:carts';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Очищает клиентские корзины, созданные более 3 дней назад';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function handle()
+    {
+        Cart::query()
+            ->whereDate('created_at', '>=', now()->subDays(2))
+            ->delete();
+    }
+}
