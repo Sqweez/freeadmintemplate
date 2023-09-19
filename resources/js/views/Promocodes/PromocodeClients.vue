8180e88268b37a57a78bbb37c1b20c42eb64925d
Alexander <sasha.katerinin@gmail.com>
2023-09-19 16:23:31 +0600

    fix

--- resources/js/views/Promocodes/PromocodeClients.vue
+++ resources/js/views/Promocodes/PromocodeClients.vue
@@ -8,10 +8,19 @@
                 label="Код с карты"
                 v-model="currentCode"
                 @keydown.enter="_createCardCode"
+                ref="element"
             />
             <v-btn text color="success" @click="_onSubmit">
                 Сохранить <v-icon>mdi-check</v-icon>
             </v-btn>
+            <p>
+                Привязанные карты:
+            </p>
+            <ul>
+                <li v-for="code of codes">
+                    {{ code }}
+                </li>
+            </ul>
         </i-card-page>
     </div>
 </template>
@@ -29,13 +38,18 @@ export default {
             return this.codes.length;
         }
     },
+    mounted() {
+        this.$refs.element.focus();
+    },
     methods: {
         _createCardCode () {
             if (this.codes.includes(this.currentCode)) {
+                this.currentCode = '';
                 return this.$toast.warning('Данная карта уже привязана');
             }
             this.codes.push(this.currentCode);
             this.currentCode = '';
+            this.$refs.element.focus();
         },
         async _onSubmit () {
             try {
