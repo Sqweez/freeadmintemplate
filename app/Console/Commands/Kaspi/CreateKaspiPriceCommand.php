63e288ce708d1906252a1fd25db73b905c2ad8ca
Alexander <sasha.katerinin@gmail.com>
2023-11-21 16:54:46 +0600

    Отключил магазин Марины на время

--- app/Console/Commands/Kaspi/CreateKaspiPriceCommand.php
+++ app/Console/Commands/Kaspi/CreateKaspiPriceCommand.php
@@ -44,7 +44,7 @@ class CreateKaspiPriceCommand extends Command
     public function handle()
     {
         ini_set('memory_limit', '512M');
-        $kaspiEntities = KaspiEntity::all();
+        $kaspiEntities = KaspiEntity::query()->where('id', 1)->get();
         foreach ($kaspiEntities as $kaspiEntity) {
             $action = new CreateKaspiPriceAction($kaspiEntity);
             $action->handle();
