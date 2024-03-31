f7b892261805fc1d51a771175c599c7c4673d0db
Alexander <sasha.katerinin@gmail.com>
2024-03-31 22:37:50 +0500

    fix

--- app/Http/Controllers/api/opt/v1/CartController.php
+++ app/Http/Controllers/api/opt/v1/CartController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\api\Opt\v1;
 
 use App\Http\Controllers\api\BaseApiController;
 use App\Repository\Opt\CartRepository;
+use Exception;
 use Illuminate\Http\Request;
 
 class CartController extends BaseApiController
@@ -21,10 +22,13 @@ class CartController extends BaseApiController
 
     }
 
+    /**
+     * @throws Exception
+     */
     public function create(Request $request)
     {
         $cartRepository = new CartRepository(auth()->user());
-        $result = $cartRepository->addToCart($request->get('product_id'), $request->get('count'));
+        $result = $cartRepository->addProductToCart($request->get('product_id'), $request->get('count'));
         return $this->respondSuccess([
             'cart' => $result,
             'user' => auth()->user(),
