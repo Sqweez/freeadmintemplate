4da4cf107862e81c45a02d61eb831da2aa91f2c6
Alexander <sasha.katerinin@gmail.com>
2024-04-01 04:17:55 +0500

    fix

--- app/Http/Controllers/api/opt/v1/OrderController.php
+++ app/Http/Controllers/api/opt/v1/OrderController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers\api\opt\v1;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\api\BaseApiController;
 use App\Repository\Opt\OrderRepository;
 use Illuminate\Http\Request;
 
-class OrderController extends Controller
+class OrderController extends BaseApiController
 {
     private OrderRepository $orderRepository;
 
@@ -15,8 +15,14 @@ class OrderController extends Controller
         $this->orderRepository = $orderRepository;
     }
 
+    /**
+     * @throws \Throwable
+     */
     public function create(Request $request)
     {
         $this->orderRepository->create(auth()->user(), $request->all());
+        return $this->respondSuccess([
+            'message' => 'Ваш заказ успешно создан, с вами свяжется менеджер уточенения деталей'
+        ]);
     }
 }
