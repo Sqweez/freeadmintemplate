494602ab239e4373be8df1f362a273748596d509
Alexander <sasha.katerinin@gmail.com>
2024-04-01 00:12:15 +0500

    fix

---
+++ app/Http/Resources/Opt/CartResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources\Opt;
+
+use App\v2\Models\UserCartItem;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/* @mixin UserCartItem */
+class CartResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'product_sku_id' => $this->product_id,
+            'product_id' => $this->product->product_id,
+            'product_image' => $this->product->product->retrieveProductThumb(),
+            'product_name' => $this->product->product->product_name,
+            'product_sub_name' => $this->product->product->attributes->pluck('attribute_value')->join(' '),
+            'quantity' => $this->product->batches->sum('quantity'),
+            'count' => $this->count,
+            'type' => $this->product->attributes->pluck('attribute_value')->join(' '),
+            'price' => $this->product->product->wholesale_prices->first()->price,
+            'id' => $this->id,
+            'cart_id' => $this->cart_id,
+            'link' => $this->product->product->getOptLink(),
+        ];
+    }
+}
--- app/v2/Models/UserCart.php
+++ app/v2/Models/UserCart.php
@@ -6,6 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
+/**
+ * App\v2\Models\UserCart
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $userable_type
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\v2\Models\UserCartItem[] $items
+ * @property-read int|null $items_count
+ * @property-read Model|\Eloquent $user
+ * @method static \Illuminate\Database\Eloquent\Builder|UserCart newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserCart newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserCart query()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserCart whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserCart whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserCart whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserCart whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserCart whereUserableType($value)
+ * @mixin \Eloquent
+ */
 class UserCart extends Model
 {
     protected $guarded = [
