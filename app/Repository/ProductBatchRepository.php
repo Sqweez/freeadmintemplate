a18dfb7b7e6db10474e7cbd1af183ebfabfcb5da
Alexander <sasha.katerinin@gmail.com>
2024-03-31 22:02:16 +0500

    fix

--- app/Repository/ProductBatchRepository.php
+++ app/Repository/ProductBatchRepository.php
@@ -20,4 +20,17 @@ class ProductBatchRepository
             ->groupBy('product_id')
             ->get();
     }
+
+    public function getProductQuantityInStore($productId, ?Store $store)
+    {
+        return ProductBatch::query()
+            ->when($store, function ($query) use ($store) {
+                return $query->where('store_id', $store->id);
+            })
+            ->where('product_id', $productId)
+            ->selectRaw('product_id, CAST(SUM(quantity) as SIGNED) as total_quantity')
+            ->where('quantity', '>', 0)
+            ->get()
+            ->sum('total_quantity');
+    }
 }
