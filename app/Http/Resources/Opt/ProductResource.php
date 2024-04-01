b85bffaf40d619cf1248e081b57df873d93097e5
Alexander <sasha.katerinin@gmail.com>
2024-04-01 07:05:42 +0500

    fix

--- app/Http/Resources/Opt/ProductResource.php
+++ app/Http/Resources/Opt/ProductResource.php
@@ -16,7 +16,7 @@ class ProductResource extends JsonResource
      */
     public function toArray($request)
     {
-        $quantity = $this->batches->sum('amount');
+        $quantity = 0;
 
         $payload =  [
             'id' => $this->id,
@@ -30,7 +30,7 @@ class ProductResource extends JsonResource
             'slug' => $this->getOptLink(),
             'brand_id' => $this->manufacturer_id,
             'quantity_type' => $this->getWholesaleQuantityType($quantity),
-            'quantity' => $quantity,
+            'quantity' => $this->sku->batches,
         ];
 
         return array_merge($payload, $this->getPrice());
