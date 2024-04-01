ebb34d2f16a8a2eb1f27ca768ea5974c8ec737f4
Alexander <sasha.katerinin@gmail.com>
2024-04-01 06:00:42 +0500

    fix

--- app/Http/Controllers/api/opt/v1/CatalogueController.php
+++ app/Http/Controllers/api/opt/v1/CatalogueController.php
@@ -111,11 +111,14 @@ class CatalogueController extends BaseApiController
 
     public function getFavorites()
     {
+        $productIds =  WholesaleFavorite::query()->where('wholesale_client_id', auth()->id())->pluck('product_id')->toArray();
         return $this->respondSuccessNoReport([
             'favorites' => ProductResource::collection(
-                Product::whereIn('id', WholesaleFavorite::query()->where('wholesale_client_id', auth()->id())->pluck('product_id')->toArray())
-                ->get()
+                Product::query()
+                    ->whereIn('id', $productIds)
+                    ->get()
             ),
+            'ids' => $productIds,
         ]);
     }
 }
