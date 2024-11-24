cfbe907dae7c093746052c88bf0a25c29377b6b2
Alexander <sasha.katerinin@gmail.com>
2024-11-24 15:52:00 +0500

    fix

--- app/Service/v2/Kaspi/BaseKaspiApiService.php
+++ app/Service/v2/Kaspi/BaseKaspiApiService.php
@@ -35,7 +35,7 @@ class BaseKaspiApiService
         $this->client = new Client([
             'base_uri' => 'https://mc.shop.kaspi.kz/mc/api/',
             'timeout'  => 10.0, // Таймаут в секундах
-            'proxy' => 'https://ironadmin.ariesdev.kz:80'
+            'proxy' => 'http://ironadmin.ariesdev.kz:80'
         ]);
     }
 
