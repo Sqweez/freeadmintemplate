b53970dfc5a3b50f2bdb81c13c3e8f96d8f63d06
Alexander <sasha.katerinin@gmail.com>
2024-12-27 22:20:24 +0500

    [fix]: kaspi price

--- app/Repository/ProductRepository.php
+++ app/Repository/ProductRepository.php
@@ -54,7 +54,7 @@ class ProductRepository
                     return [
                         'available' => collect($product['batches'])->filter(function ($item) use ($store) {
                             return $item['store_id'] === $store['store_id'];
-                        })->sum('quantity')
+                        })->sum('quantity'),
                         'storeId' => 'PP' . ($store['kaspi_store_id'])];
                 }),
                 'quantities' => collect($stores)->map(function ($store) use ($product) {
